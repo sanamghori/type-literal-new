3d5e69cbeca571d6db7b2ac21a5ef2934a40dd49
sanamghori <161497237+sanamghori@users.noreply.github.com>
2024-09-19 18:54:53 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+"use strict";
+//  type literal// is main hum is k sath| jo value assgine krty hy wo hi nechy valid 
+//hoga koch or valid nhi hoga or print main last value print hogi
+let traffics;
+traffics = "red";
+traffics = "blue";
+traffics = "green";
+console.log(traffics);
+// type union or type literal main ye difference hota hy lite main value assgine krty
+//union mian type assgine kerte hy jese string number union main value koch bhi ho data
+// type sm honi chy
+// type union
+let fruitsbascit;
+fruitsbascit = "apple",
+    fruitsbascit = 6,
+    fruitsbascit = "maonge",
+    fruitsbascit = true,
+    console.log(fruitsbascit);
